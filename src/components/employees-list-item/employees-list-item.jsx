6ad7729f108df1631b2ed1ef730b50629f49bc46
Andrey <bok20@rambler.ru>
2022-06-22 01:36:04 +0300

    first commit

---
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -0,0 +1,49 @@
+import "./emploees-list-item.css";
+
+const EmpoyeesListItem = (props) => {
+  const {
+    name,
+    salary,
+    onDelete,
+    onToggleProp,
+    increase,
+    rise,
+  } = props;
+  const classNames = [
+    "list-group-item",
+    "d-flex",
+    "justify-content-between",
+    increase ? "increase" : "",
+    rise ? "like" : "",
+  ]
+    .filter((className) => className?.length > 0)
+    .join(" ");
+  return (
+    <li className={classNames}>
+      <span className="list-group-item-label" onClick={onToggleProp} data-toggle="rise">
+        {name}
+      </span>
+      <input
+        type="text"
+        className="list-group-item-input"
+        defaultValue={salary + "$"}
+      />
+      <div className="d-flex justify-content-center align-items-center">
+        <button
+          type="button"
+          className="btn-cookie btn-sm "
+          onClick={onToggleProp}
+          data-toggle="increase"
+        >
+          <i className="fas fa-cookie"></i>
+        </button>
+
+        <button type="button" className="btn-trash btn-sm " onClick={onDelete}>
+          <i className="fas fa-trash"></i>
+        </button>
+        <i className="fas fa-star"></i>
+      </div>
+    </li>
+  );
+};
+export default EmpoyeesListItem;
